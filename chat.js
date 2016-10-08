079308ecb11759f67e9d17acf696f9a6be91ae69
danielfablius <danielfablius@gmail.com>
2016-10-08 15:50:13 +0700

    init

---
+++ chat.js
@@ -0,0 +1,73 @@
+
+var socket = io()
+var sessionId
+
+function submitfunction(){
+  var from = $('#user').val()
+  var message = $('#m').val()
+  if(message != '') {
+  // socket.emit('chatMessage', from, message);
+  socket.emit('sendToUser', sessionId, from, message)
+}
+
+$('#m').val('').focus();
+  return false;
+}
+
+// function notifyTyping() { 
+//   var user = $('#user').val();
+//   socket.emit('notifyUser', user);
+// }
+
+socket.on('rooms_join',function(message){
+  console.log(socket.io.engine.id, message)
+})
+
+socket.on('connect',function(message){
+  console.log(socket.io.engine.id)
+})
+
+socket.on('message', function(message){
+  sessionId = message
+})
+
+socket.on('sendToUser', function(from, msg) {
+  var me = $('#user').val()
+  var color = (from == me) ? 'green' : '#009afd'
+  var from = (from == me) ? 'Me' : from
+  $('#messages').append('<li><b style="color:' + color + '">' + from + '</b>: ' + msg + '</li>')
+})
+
+socket.on('chatMessage', function(from, msg){
+  var me = $('#user').val()
+  var color = (from == me) ? 'green' : '#009afd'
+  var from = (from == me) ? 'Me' : from
+  $('#messages').append('<li><b style="color:' + color + '">' + from + '</b>: ' + msg + '</li>')
+});
+
+// socket.on('notifyUser', function(user){
+//   var me = $('#user').val();
+//   if(user != me) {
+//     $('#notifyUser').text(user + ' is typing ...');
+//   }
+//   setTimeout(function(){ $('#notifyUser').text(''); }, 10000);;
+// });
+$(document).ready(function(){
+  // push socket ke cache
+  var name = makeid()
+  $('#user').val(name)
+  socket.emit('chatMessage', 'System', '<b>' + name + '</b> has joined the discussion')
+});
+
+function makeid() {
+  // ganti jadi input user menggunakan sweetalert biar so sweet :3
+  // http://t4t5.github.io/sweetalert/
+  // minta masukkan username
+  var text = ""
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+ 
+  for( var i=0; i < 5; i++ ) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length))
+  }
+  return text;
+}
---
+++ index.js
@@ -0,0 +1,113 @@
+var app = require('express')()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+var path = require('path')
+var clients = []
+
+ips = []
+waiting_rooms = []
+rooms =
+  [
+
+  ]
+
+// Initialize appication with route / (that means root of the application)
+app.get('/', function(req, res) {
+  var express=require('express');
+  app.use(express.static(path.join(__dirname)));
+  // res.sendFile(path.join(__dirname, '../test', 'index.html'));
+  res.sendFile(__dirname + '/index.html')
+});
+ 
+// Register events on socket connection
+io.on('connection', function(socket){
+  console.info('New client connected (id = ' + socket.id + ').')
+  clients.push(socket)
+  socket.send(socket.id)
+  io.sockets.connected[socket.id].emit('connect')
+  // setTimeout(function() {console.log("t1")}, 5000);
+  // console.log(socket)
+  
+  var address = socket.handshake.address;
+  console.log('New connection from ' + address);
+  if(ips.indexOf(address)==-1)
+  {
+    ips.push(address)
+    waiting_rooms.push(socket.id)
+    console.log("ip terdaftar :"+ips)
+  }
+  console.log("waitinglist:"+waiting_rooms)
+
+  if(waiting_rooms.length > 1){
+    num = Math.floor((Math.random() * waiting_rooms.length));
+    p1 = waiting_rooms[num]
+    waiting_rooms.splice(num,1)
+    console.log(num,waiting_rooms.length)
+    num = Math.floor((Math.random() * waiting_rooms.length));
+    p2 = waiting_rooms[num]
+    waiting_rooms.splice(num,1)
+    console.log(num,waiting_rooms.length)
+    d = new Date()
+    room_id = "_"+d.getTime()
+    rooms[room_id] = {p1:p1,p2:p2}
+    if(io.sockets.connected[p1])io.sockets.connected[p1].emit('rooms_join',{room_id:room_id})
+    if(io.sockets.connected[p2])io.sockets.connected[p2].emit('rooms_join',{room_id:room_id})
+
+  }
+      // console.log(rooms)
+  // built in
+    console.log("waitinglist 2 :"+waiting_rooms)
+
+  socket.on('disconnect', function() {
+    console.log("before_dc:" +waiting_rooms)
+    ix = waiting_rooms.indexOf(socket.id)
+    console.log(ix) 
+    address = socket.handshake.address
+    // console.log(address)
+    
+    iadd = ips.indexOf(address)
+    if(iadd>=0)ips.splice(iadd,1)
+    // console.log(ips) 
+    // console.log(iadd) 
+    // console.log(waiting_rooms)
+    if(ix>=0)waiting_rooms.splice(ix,1)
+    // console.log("rooms2" +waiting_rooms)
+    for(x in rooms){
+      if(rooms[x].p1==socket.id || rooms[x].p2==socket.id){
+        // console.log(rooms)
+        delete rooms[x]
+        // console.log(rooms)
+      }
+    }
+    var index = clients.indexOf(socket)
+    if (index != -1) {
+      clients.splice(index, 1)
+      console.info('Client gone (id=' + socket.id + ').')
+    }
+
+  })
+
+  for (i in clients) {
+    console.info('i=' + i + ", id=" + clients[i].id)
+  }
+
+  socket.on('chatMessage', function(from, msg){
+    io.emit('chatMessage', from, msg);  // emit ke semua user
+  });
+
+  socket.on('sendToUser', function(sessionId, from, msg){
+    console.info('Send msg=' + msg + " to id=" + clients[0].id)
+    io.sockets.connected[clients[0].id].emit('sendToUser', from, msg)
+    if (clients[0].id != sessionId)
+      io.sockets.connected[sessionId].emit('sendToUser', from, msg)
+  })
+
+  // socket.on('notifyUser', function(user){
+  //   io.emit('notifyUser', user);
+  // });
+});
+ 
+// Listen application request on port 3000
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
